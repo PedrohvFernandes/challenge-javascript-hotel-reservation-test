e8b2b6e4245e39f8c435d1df7b70c624565487f5
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-05-13 00:56:23 -0300

    Melhorando o codigo

--- src/main.js
+++ src/main.js
@@ -40,119 +40,58 @@ const HOTEIS = [
 ];
 
 function getCheapestHotel(input) {
+  //clientes: separador -> :   não pego o restante depois do separador -> [0]
+  const clientes = input.split(":")[0];
   // Armazenando a soma dos preços de todos os hoteis
   let custoHoteis = [0, 0, 0];
 
-  //clientes: separador -> :   não pego o restante depois do separador -> [0]
-  const clientes = input.split(":")[0];
-  // Pegandos os meses que vem do test
-  const datas1 = input.split(":")[1].split(",")[0].split("(")[1].split(")")[0];
-  const datas2 = input.split(":")[1].split(",")[1].split("(")[1].split(")")[0];
-  const datas3 = input.split(":")[1].split(",")[2].split("(")[1].split(")")[0];
-
-  //Primeiro descobrir quem é o cliente
-  if (clientes === "Regular") {
-    // Depois descobrir se é final de semana ou não
-    if (
-      datas1 === "mon" ||
-      datas1 === "tues" ||
-      datas1 === "wed" ||
-      datas1 === "thur" ||
-      datas1 === "fri"
-    ) {
-      custoHoteis[0] += HOTEIS[0].naSemana.regular;
-      custoHoteis[1] += HOTEIS[1].naSemana.regular;
-      custoHoteis[2] += HOTEIS[2].naSemana.regular;
-    } else if (datas1 === "sat" || datas1 === "sun") {
-      custoHoteis[0] += HOTEIS[0].noFinalSemana.regular;
-      custoHoteis[1] += HOTEIS[1].noFinalSemana.regular;
-      custoHoteis[2] += HOTEIS[2].noFinalSemana.regular;
-    }
-
-    if (
-      datas2 === "mon" ||
-      datas2 === "tues" ||
-      datas2 === "wed" ||
-      datas2 === "thur" ||
-      datas2 === "fri"
-    ) {
-      custoHoteis[0] += HOTEIS[0].naSemana.regular;
-      custoHoteis[1] += HOTEIS[1].naSemana.regular;
-      custoHoteis[2] += HOTEIS[2].naSemana.regular;
-    } else if (datas2 === "sat" || datas2 === "sun") {
-      custoHoteis[0] += HOTEIS[0].noFinalSemana.regular;
-      custoHoteis[1] += HOTEIS[1].noFinalSemana.regular;
-      custoHoteis[2] += HOTEIS[2].noFinalSemana.regular;
-    }
-
-    if (
-      datas3 === "mon" ||
-      datas3 === "tues" ||
-      datas3 === "wed" ||
-      datas3 === "thur" ||
-      datas3 === "fri"
-    ) {
-      custoHoteis[0] += HOTEIS[0].naSemana.regular;
-      custoHoteis[1] += HOTEIS[1].naSemana.regular;
-      custoHoteis[2] += HOTEIS[2].naSemana.regular;
-    } else if (datas3 === "sat" || datas3 === "sun") {
-      custoHoteis[0] += HOTEIS[0].noFinalSemana.regular;
-      custoHoteis[1] += HOTEIS[1].noFinalSemana.regular;
-      custoHoteis[2] += HOTEIS[2].noFinalSemana.regular;
+  let datas;
+  for (i = 0; i < 3; i++) {
+    // Pegando os meses que vem do test
+    // const datas = input.split(":")[1].split(",")[i].split("(")[1].split(")")[0];
+    // const datas1 = input.split(":")[1].split(",")[0].split("(")[1].split(")")[0];
+    // const datas2 = input.split(":")[1].split(",")[1].split("(")[1].split(")")[0];
+    // const datas3 = input.split(":")[1].split(",")[2].split("(")[1].split(")")[0];
+    datas = input.split(":")[1].split(",")[i].split("(")[1].split(")")[0];
+    console.log(datas);
+    //Primeiro descobrir quem é o cliente
+    if (clientes === "Regular") {
+      // Depois descobrir se é final de semana ou não
+      if (
+        datas === "mon" ||
+        datas === "tues" ||
+        datas === "wed" ||
+        datas === "thur" ||
+        datas === "fri"
+      ) {
+        custoHoteis[0] += HOTEIS[0].naSemana.regular;
+        custoHoteis[1] += HOTEIS[1].naSemana.regular;
+        custoHoteis[2] += HOTEIS[2].naSemana.regular;
+      } else if (datas === "sat" || datas === "sun") {
+        custoHoteis[0] += HOTEIS[0].noFinalSemana.regular;
+        custoHoteis[1] += HOTEIS[1].noFinalSemana.regular;
+        custoHoteis[2] += HOTEIS[2].noFinalSemana.regular;
+      }
+    } else if (clientes === "Rewards") {
+      if (
+        datas === "mon" ||
+        datas === "tues" ||
+        datas === "wed" ||
+        datas === "thur" ||
+        datas === "fri"
+      ) {
+        custoHoteis[0] += HOTEIS[0].naSemana.reward;
+        custoHoteis[1] += HOTEIS[1].naSemana.reward;
+        custoHoteis[2] += HOTEIS[2].naSemana.reward;
+      } else if (datas === "sat" || datas === "sun") {
+        custoHoteis[0] += HOTEIS[0].noFinalSemana.reward;
+        custoHoteis[1] += HOTEIS[1].noFinalSemana.reward;
+        custoHoteis[2] += HOTEIS[2].noFinalSemana.reward;
+      }
     }
   }
 
-  if (clientes === "Rewards") {
-    if (
-      datas1 === "mon" ||
-      datas1 === "tues" ||
-      datas1 === "wed" ||
-      datas1 === "thur" ||
-      datas1 === "fri"
-    ) {
-      custoHoteis[0] += HOTEIS[0].naSemana.reward;
-      custoHoteis[1] += HOTEIS[1].naSemana.reward;
-      custoHoteis[2] += HOTEIS[2].naSemana.reward;
-    } else if (datas1 === "sat" || datas1 === "sun") {
-      custoHoteis[0] += HOTEIS[0].noFinalSemana.reward;
-      custoHoteis[1] += HOTEIS[1].noFinalSemana.reward;
-      custoHoteis[2] += HOTEIS[2].noFinalSemana.reward;
-    }
-
-    if (
-      datas2 === "mon" ||
-      datas2 === "tues" ||
-      datas2 === "wed" ||
-      datas2 === "thur" ||
-      datas2 === "fri"
-    ) {
-      custoHoteis[0] += HOTEIS[0].naSemana.reward;
-      custoHoteis[1] += HOTEIS[1].naSemana.reward;
-      custoHoteis[2] += HOTEIS[2].naSemana.reward;
-    } else if (datas2 === "sat" || datas2 === "sun") {
-      custoHoteis[0] += HOTEIS[0].noFinalSemana.reward;
-      custoHoteis[1] += HOTEIS[1].noFinalSemana.reward;
-      custoHoteis[2] += HOTEIS[2].noFinalSemana.reward;
-    }
-
-    if (
-      datas3 === "mon" ||
-      datas3 === "tues" ||
-      datas3 === "wed" ||
-      datas3 === "thur" ||
-      datas3 === "fri"
-    ) {
-      custoHoteis[0] += HOTEIS[0].naSemana.reward;
-      custoHoteis[1] += HOTEIS[1].naSemana.reward;
-      custoHoteis[2] += HOTEIS[2].naSemana.reward;
-    } else if (datas3 === "sat" || datas3 === "sun") {
-      custoHoteis[0] += HOTEIS[0].noFinalSemana.reward;
-      custoHoteis[1] += HOTEIS[1].noFinalSemana.reward;
-      custoHoteis[2] += HOTEIS[2].noFinalSemana.reward;
-    }
-  }
-
-  // Comparo os preços e a classe
+  // Comparo os preços e a classificação dos hoteis
   if (custoHoteis[0] < custoHoteis[1] && custoHoteis[0] < custoHoteis[2]) {
     return HOTEIS[0].name;
   } else if (
